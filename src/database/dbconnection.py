35b6e5a705e318c18384b262e0898989e61b93f7
Jhonatan <morales.hernandez.jhonatan00@gmail.com>
2024-08-14 21:39:28 -0500

    dbconnection

---
+++ src/database/dbconnection.py
@@ -0,0 +1,23 @@
+from decouple import config
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker
+
+
+def getconnection():
+    dialect = config('PGDIALECT')
+    user = config('PGUSER')
+    passwd = config('PGPASSWD')
+    host = config('PGHOST')
+    port = config('PGPORT')
+    db = config('PGDB')
+    
+    url = f"{dialect}://{user}:{passwd}@{host}:{port}/{db}"
+    
+    try:
+        engine = create_engine(url)
+        print(f'Conected successfully to database {db}!')
+        return engine
+    except SQLAlchemyError as e:
+        print(f'Error: {e}')
+getconnection()
