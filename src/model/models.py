ac12e9c8ceca07b04328db344ff0ebe03e8266f3
Manuel <manuel.gruezo@uao.edu.co>
2024-08-19 23:56:54 -0500

    Guardando cambios en models.py antes del pull

--- src/model/models.py
+++ src/model/models.py
@@ -1,24 +1,37 @@
-from sqlalchemy import Column, Integer, String, Date
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Float, Date, Boolean
+from sqlalchemy.ext.declarative import declarative_base
 
-BASE = declarative_base()
-MAX_STRING_SIZE = 100
+Base = declarative_base()
 
-class Staging(BASE):
-    
-    __tablename__ = 'Staging'
+class CardioTrain(Base):
+    __tablename__ = 'cardio_train'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
-    WorkYear = Column(Integer, nullable=False)
-    ExperienceLevel = Column(String(MAX_STRING_SIZE), nullable=False)
-    EmploymentType = Column(String(MAX_STRING_SIZE), nullable=False)
-    JobTitle = Column(String(MAX_STRING_SIZE), nullable=False)
-    Salary =  Column(Integer, primary_key=True, autoincrement=True)
-    SalaryCurrency = Column(String(MAX_STRING_SIZE), nullable=False)
-    SalaryInUSD =  Column(Integer, primary_key=True, autoincrement=True)
-    EmployeeResidence = Column(String(MAX_STRING_SIZE), nullable=False)
-    RemoteRatio =  Column(Integer, primary_key=True, autoincrement=True)
-    CompanyLocation = Column(String(MAX_STRING_SIZE), nullable=False)
-    CompanySize = Column(String(MAX_STRING_SIZE), nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(Integer, nullable=False)
+    height = Column(Integer, nullable=False)
+    weight = Column(Float, nullable=False)
+    ap_hi = Column(Integer, nullable=False)  # Presión arterial sistólica
+    ap_lo = Column(Integer, nullable=False)  # Presión arterial diastólica
+    cholesterol = Column(Integer, nullable=False)
+    gluc = Column(Integer, nullable=False)
+    smoke = Column(Boolean, nullable=False)
+    alco = Column(Boolean, nullable=False)
+    active = Column(Boolean, nullable=False)
+    cardio = Column(Boolean, nullable=False)
 
-    def __str__ (self):
-        return f" Table: {self.Staging.__table__}"
+    def __str__(self):
+        return f"<CardioTrain(id={self.id}, age={self.age}, gender={self.gender})>"
+
+
+class CauseOfDeaths(Base):
+    __tablename__ = 'cause_of_deaths'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    country = Column(String, nullable=False)
+    year = Column(Integer, nullable=False)
+    cause = Column(String, nullable=False)
+    deaths = Column(Integer, nullable=False)
+
+    def __str__(self):
+        return f"<CauseOfDeaths(id={self.id}, country={self.country}, year={self.year})>"
