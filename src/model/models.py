36106a81840e6fb2a9761604408515f7746a9842
Jhonatan <morales.hernandez.jhonatan00@gmail.com>
2024-08-21 04:01:38 -0500

    database processed

--- src/model/models.py
+++ src/model/models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, Integer, String, Float
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
-Base = declarative_base()
+BASE = declarative_base()
+MAX_STRING_SIZE = 20
 
-class CardioTrain(Base):
+class CardioTrain(BASE):
     __tablename__ = 'CardioTrain'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -24,8 +25,8 @@ class CardioTrain(Base):
         return f"table create"
 
 
-class CauseOfDeaths(Base):
-    __tablename__ = 'cause_of_deaths'
+class CauseOfDeaths(BASE):
+    __tablename__ = 'CauseOfDeaths'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     country = Column(String, nullable=False)
@@ -35,3 +36,51 @@ class CauseOfDeaths(Base):
 
     def __str__(self):
         return f"<CauseOfDeaths(id={self.id}, country={self.country}, year={self.year})>"
+
+
+class CardioTrainNormalize(BASE):
+    __tablename__ = 'CardioTrainNormalize'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    age = Column(Integer, nullable=False)
+    gender = Column(String(MAX_STRING_SIZE), nullable=False)
+    height = Column(Integer, nullable=False)
+    weight = Column(Float, nullable=False)
+    IMC = Column(Float, nullable=False)
+    ap_hi = Column(Integer, nullable=False)  # Presión arterial sistólica
+    ap_lo = Column(Integer, nullable=False)  # Presión arterial diastólica
+    cholesterolID = Column(Integer, ForeignKey('CholesterolTypes.id'), nullable=False)
+    glucID = Column(Integer, ForeignKey('GlucoseTypes.id'), nullable=False)
+    smoke = Column(Integer, nullable=False)
+    alco = Column(Integer, nullable=False)
+    active = Column(Integer, nullable=False)
+    cardio = Column(Integer, nullable=False)
+
+    cholesterol = relationship("CholesterolTypes", back_populates="cardio")
+    glucose = relationship("GlucoseTypes", back_populates="cardio")
+
+    def __str__(self):
+        return f"table create"
+
+
+class CholesterolTypes(BASE):
+    __tablename__ = 'CholesterolTypes'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    CholesterolLevel = Column(String(MAX_STRING_SIZE), nullable=False)
+    
+    cardio = relationship("CardioTrainNormalize", back_populates="cholesterol")
+
+    def __str__ (self):
+        return f" Table: {self.Countries.__table__}"
+
+class GlucoseTypes(BASE):
+    __tablename__ = 'GlucoseTypes'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    GlucLevel = Column(String(MAX_STRING_SIZE), nullable=False)
+    
+    cardio = relationship("CardioTrainNormalize", back_populates="glucose")
+
+    def __str__ (self):
+        return f" Table: {self.Countries.__table__}"
+    
+
--- transform/TransformData.py
+++ transform/TransformData.py
@@ -1,7 +1,78 @@
 import pandas as pd
 import numpy as np
+from math import floor
 
 class DataTransform:
     def __init__(self, file):
 
         self.df = pd.read_csv(file, sep=";")
+    
+    def  gender_by_category(self) -> None:
+        self.gender_to_category = {
+            1 : 'Female',
+            2 : 'Male'
+        }
+        self.df['gender'] = self.df['gender'].map(self.gender_to_category)
+    
+    def cholesterol_by_category(self) -> None:
+        self.cholesterol_to_category = {
+            1: 'normal',
+            2: 'above normal',
+            3: 'well above normal'
+        }
+        self.df['cholesterol'] = self.df['cholesterol'].map(self.cholesterol_to_category)
+    
+    def gluc_by_category(self) -> None:
+
+        self.gluc_to_category = {
+            1: 'normal',
+            2: 'above normal',
+            3: 'well'
+        }
+        self.df['gluc'] = self.df['gluc'].map(self.gluc_to_category)
+
+    def IMC(self) -> None:
+
+        self.df['IMC'] = round(self.df['weight'] / (self.df['height'] ** 2), 2)
+        self.df['IMC'] = self.df['IMC'].astype(float)
+
+
+    def days_to_age(self) -> None:
+
+        self.df['age'] = np.floor(self.df['age'] / 365.3)
+        self.df['age'] = self.df['age'].astype(int)
+        
+    def nomalize_gluc(self) -> pd.DataFrame:
+
+        unique_gluc = self.df['gluc'].unique()
+
+        gluc_df = pd.DataFrame({
+            'id': range(1, len(unique_gluc) + 1),
+            'GlucLevel': unique_gluc
+        })
+
+        gluc_map = dict(zip(unique_gluc, gluc_df['id']))
+
+        self.df['glucID'] = self.df['gluc'].map(gluc_map)
+
+        self.df.drop(columns=['gluc'], inplace=True)
+
+        return gluc_df
+    
+    
+    def normalize_cholesterol(self) -> pd.DataFrame:
+
+        unique_cholesterol = self.df['cholesterol'].unique()
+
+        cholesterol_df = pd.DataFrame({
+            'id': range(1, len(unique_cholesterol) + 1),
+            'CholesterolLevel': unique_cholesterol
+        })
+
+        cholesterol_map = dict(zip(unique_cholesterol, cholesterol_df['id']))
+
+        self.df['cholesterolID'] = self.df['cholesterol'].map(cholesterol_map)
+
+        self.df.drop(columns=['cholesterol'], inplace=True)
+
+        return cholesterol_df
